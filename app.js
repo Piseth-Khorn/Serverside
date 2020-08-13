c1d2016233077518191fb6c02a8f46ea3636492e
Dev1 <piseth.kh328@gmail.com>
2020-08-13 14:27:23 +0700

    update Auth

--- app.js
+++ app.js
@@ -13,25 +13,21 @@ const saltRounds = 10;
 const myPlainTextPassword = 's0/\/\P4$$w0rD';
 const someOtherPainTextPassword = 'not_bacon';
 //const fileUpload = require('./config/fileupload');
-const cors = require('cors');
+const cors = require('cors'); 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
 const con = mongoose.connection;
 con.once('open', function () {
     console.log('connected...');
 });
-
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-//enables cors
-app.use(cors({
-    'allowedHeaders': ['sessionId', 'Content-Type'],
-    'exposedHeaders': ['sessionId'],
-    'origin': '*',
-    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    'preflightContinue': false
-}));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type, Accept");
+    next();
+});
 app.get('/', (req, res, next) => {
     res.sendFile('./ui/index.html', { root: __dirname });
 });
